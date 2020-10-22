f0cde1e5d6dc55ae6d3b364f92ea149859fb7075
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-10-22 11:39:44 +0200

    Add template project

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
+// import Functional from './src1/Functional';
+import Main from './srcETradeApp/Main';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => Main);
