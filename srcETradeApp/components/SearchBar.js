80f0d4335f3b484b4f2c20e9c85f878da7382d01
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-10-22 11:48:00 +0200

    Add SearcBar.js

---
+++ srcETradeApp/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SearchBar = (props) => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+export { SearchBar };
